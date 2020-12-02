07907c183bd5a51bc9e117a0cb9892d8e12afcf2
shalala0115 <shalala0115@gmail.com>
2020-12-02 10:58:54 -0800

    Updates

---
+++ precache-manifest.b17ac071cf5ddd39fb4a68ae80d7e642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c49099ff8d3a773a26c03f856c3d59",
+    "url": "/GreenG/index.html"
+  },
+  {
+    "revision": "550e97ccd590e44e22f3",
+    "url": "/GreenG/static/css/main.253fb6b2.chunk.css"
+  },
+  {
+    "revision": "3135ce5a482b24038a73",
+    "url": "/GreenG/static/js/2.e78548f0.chunk.js"
+  },
+  {
+    "revision": "f722b121680cd2cfb8c4cd074bb5a0a8",
+    "url": "/GreenG/static/js/2.e78548f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550e97ccd590e44e22f3",
+    "url": "/GreenG/static/js/main.cb22ba48.chunk.js"
+  },
+  {
+    "revision": "aba758a1e882cbfd7eae",
+    "url": "/GreenG/static/js/runtime-main.1b600b02.js"
+  }
+]);
